795371c31a8cff40bdc996b44dc867afad8860e9
Waldemar Tomme <waldetomme@gmail.com>
2023-02-10 13:58:32 +0100

    Use record type

--- DarkLink.RoslynHelpers.AttributeGenerator/GenerateAttributeData.cs
+++ DarkLink.RoslynHelpers.AttributeGenerator/GenerateAttributeData.cs
@@ -3,24 +3,9 @@
 namespace DarkLink.RoslynHelpers.AttributeGenerator;
 
 [GenerateAttribute(AttributeTargets.Class, Namespace = "DarkLink.RoslynHelpers", Name = "GenerateAttributeAttribute")]
-internal partial class GenerateAttributeData
-{
-    public GenerateAttributeData(AttributeTargets validOn, bool allowMultiple = false, bool inherited = true, string? @namespace = default, string? name = default)
-    {
-        ValidOn = validOn;
-        AllowMultiple = allowMultiple;
-        Inherited = inherited;
-        Namespace = @namespace;
-        Name = name;
-    }
-
-    public bool AllowMultiple { get; }
-
-    public bool Inherited { get; }
-
-    public string? Name { get; }
-
-    public string? Namespace { get; }
-
-    public AttributeTargets ValidOn { get; }
-}
+internal partial record GenerateAttributeData(
+    AttributeTargets ValidOn,
+    bool AllowMultiple = false,
+    bool Inherited = true,
+    string? Namespace = default,
+    string? Name = default);
